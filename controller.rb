b1ee8eb6b36d67829f6d617f837dc2313ddf0fa3
Rory MacGregor <you@example.com>
2019-03-21 13:36:00 +0000

    Built erb files.

--- controller.rb
+++ controller.rb
@@ -1,1 +1,24 @@
 require('pry-byebug')
+
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('./models/student.rb')
+also_reload('./models/*')
+
+get '/students' do #INDEX
+  @students = Student.find_all()
+  erb(:index)
+end
+
+get '/students/new' do  #NEW
+  erb(:new)
+end
+
+post '/students' do
+  @student = Student.new( params )
+  @student.save()
+  erb( :create )
+end
+
+# binding.pry
+# nil
